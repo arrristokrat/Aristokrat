68a6c9cef0518a56a899d3ba37537ef6a167fd3d
Petro.Dmytriv <petro.dmytriv@pdmytriv-Macbook-Pro.local>
2022-08-21 21:04:43 +0300

    adds images

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div><img src="images/first.svg" alt="first"/></div>
+      <div><img src="images/second.svg" alt="second"/></div>
+      <div><img src="images/1.svg" alt="1"/></div>
+      <div><img src="images/2.svg" alt="2"/></div>
+      <div><img src="images/3.svg" alt="3"/></div>
+      <div><img src="images/4.svg" alt="4"/></div>
+      <div><img src="images/5.svg" alt="5"/></div>
+      <div><img src="images/6.svg" alt="6"/></div>
+      <div><img src="images/7.svg" alt="7"/></div>
     </div>
   );
 }
